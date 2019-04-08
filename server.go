d493fbbb775ae72034c6ced12ccd83d1da084cdd
Jesse Kuang <jkuang@sh163.net>
2019-04-08 23:59:45 +0800

    revamp server and serverRaw

--- server.go
+++ server.go
@@ -5,41 +5,16 @@ package MoldUDP
 import (
 	"net"
 	"runtime"
-	"sync/atomic"
 	"time"
-)
-
-const (
-	maxUDPsize   = 1472
-	heartBeatInt = 2
-	maxGoes      = 512
-	maxWindow    = 120000
-	PPms         = 100 // packets per ms
 )
 
 // Server struct for MoldUDP server
 //	Running		bool
 //	Session		session for all messages
 type Server struct {
-	Session       string
-	dst           net.UDPAddr
-	conn          *net.UDPConn
-	connReq       *net.UDPConn
-	PPms          int
-	Running       bool
-	endSession    bool
-	seqNo         uint64
-	endTime       int64
-	waits         int // wait for 5 seconds end of session
-	nRecvs, nSent int
-	nError        int
-	nResent       int32
-	nGoes         int32
-	nMaxGoes      int
-	nHeartBB      int
-	nSleep        int
-	msgs          []Message
-	buff          []byte
+	conn *net.UDPConn
+	dst  net.UDPAddr
+	ServerBase
 }
 
 func (c *Server) Close() error {
@@ -48,46 +23,30 @@ func (c *Server) Close() error {
 	}
 	err := c.conn.Close()
 	c.conn = nil
-	if c.connReq != nil {
-		c.connReq.Close()
-		c.connReq = nil
-	}
+	c.ServerBase.Close()
 	return err
-}
-
-func (c *Server) EndSession(nWaits int) {
-	c.endSession = true
-	if nWaits > c.waits {
-		c.waits = nWaits
-	}
-}
-
-func (c *Server) FeedMessages(feeds []Message) {
-	c.msgs = append(c.msgs, feeds...)
 }
 
 func NewServer(udpAddr string, port int, ifName string, bLoop bool) (*Server, error) {
 	var err error
-	server := Server{seqNo: 1, waits: 5, PPms: PPms}
-	// sequence number is 1 based
-	server.dst.IP = net.ParseIP(udpAddr)
-	server.dst.Port = port
-	if !server.dst.IP.IsMulticast() {
-		log.Info(server.dst.IP, " is not multicast IP")
-		server.dst.IP = net.IPv4(224, 0, 0, 1)
-	}
-	var ifn *net.Interface
-	if ifName != "" {
-		if ifn, err = net.InterfaceByName(ifName); err != nil {
-			log.Errorf("Ifn(%s) error: %v\n", ifName, err)
-			ifn = nil
-		}
-	}
+	server := Server{}
+	server.ServerBase.init(udpAddr, port)
+	server.dst.IP = server.dstIP
+	server.dst.Port = server.dstPort
 	var fd int = -1
 	laddr := net.UDPAddr{IP: net.IPv4(0, 0, 0, 0), Port: port}
 	if bLoop {
 		// let system allc port
 		laddr.Port = 0
+	}
+	var ifn *net.Interface
+	if ifName != "" {
+		if ifnn, err := net.InterfaceByName(ifName); err != nil {
+			log.Errorf("Ifn(%s) error: %v\n", ifName, err)
+			ifn = nil
+		} else {
+			ifn = ifnn
+		}
 	}
 	server.conn, err = net.ListenUDP("udp", &laddr)
 	if err != nil {
@@ -99,17 +58,9 @@ func NewServer(udpAddr string, port int, ifName string, bLoop bool) (*Server, er
 		log.Error("Get UDPConn fd", err)
 	}
 	log.Info("Server listen", server.conn.LocalAddr())
-	laddr.Port++
-	server.connReq, err = net.ListenUDP("udp", &laddr)
-	if err != nil {
-		log.Error("can't listen on request port")
-		server.connReq = nil
-	} else {
-		log.Info("Request Server listen", server.connReq.LocalAddr())
-	}
 	ReserveSendBuf(fd)
 	/*
-		if err := JoinMulticast(fd, server.dst.IP, ifn); err != nil {
+		if err := JoinMulticast(fd, server.dstIP, ifn); err != nil {
 			log.Info("add multicast group", err)
 		}
 	*/
@@ -121,155 +72,7 @@ func NewServer(udpAddr string, port int, ifName string, bLoop bool) (*Server, er
 			log.Info("set multicast loopback", err)
 		}
 	}
-	server.buff = make([]byte, 2048)
-	server.Running = true
 	return &server, nil
-}
-
-type hostControl struct {
-	remote   net.UDPAddr
-	bEnd     bool
-	seqAcked uint64
-	seqNext  uint64 // max nak sequence
-	running  int32
-}
-
-// RequestLoop		go routine process request retrans
-func (c *Server) RequestLoop() {
-	hostMap := map[string]*hostControl{}
-	var nResends int
-
-	doReq := func(hc *hostControl) {
-		//seqNo uint64, cnt uint16, remoteA net.UDPAddr
-		// only retrans one UDP packet
-		// proce reTrans
-		var buff [maxUDPsize]byte
-		seqNo := hc.seqAcked
-
-		defer atomic.AddInt32(&c.nGoes, -1)
-		nResends++
-		if (nResends & 127) == 0 {
-			log.Infof("Resend packets to %s Seq: %d -- %d", hc.remote.IP,
-				seqNo, hc.seqNext)
-		}
-		sHead := Header{Session: c.Session, SeqNo: seqNo}
-		for seqNo < atomic.LoadUint64(&hc.seqNext) {
-			lastS := int(atomic.LoadUint64(&hc.seqNext))
-			msgCnt, bLen := Marshal(buff[headSize:], c.msgs[seqNo-1:lastS-1])
-			if msgCnt == 0 && c.endSession {
-				// end of Session
-				break
-			} else {
-				sHead.MessageCnt = uint16(msgCnt)
-			}
-			if err := EncodeHead(buff[:headSize], &sHead); err != nil {
-				log.Error("EncodeHead for proccess reTrans", err)
-				continue
-			}
-			atomic.AddInt32(&c.nResent, 1)
-			if _, err := c.connReq.WriteToUDP(buff[:headSize+bLen],
-				&hc.remote); err != nil {
-				log.Error("Res WriteToUDP", hc.remote, err)
-				break
-			}
-			seqNo += uint64(msgCnt)
-			if seqNo >= uint64(len(c.msgs)) {
-				break
-			}
-			sHead.SeqNo = seqNo
-			// system Sleep delay 50us about, so about 100us sleep
-			// or changed to Sleep 50us
-			time.Sleep(time.Microsecond * 50)
-		}
-		atomic.StoreInt32(&hc.running, 0)
-		// ony recover lost last packet and endSession
-		// using endSession Hearbeat packet to indicate endSession
-		if c.endSession && int(c.seqNo) >= len(c.msgs) && !hc.bEnd {
-			hc.bEnd = true
-			sHead.SeqNo = seqNo
-			// send endSession as well
-			sHead.MessageCnt = 0xffff
-			//log.Info("Retrans endSession, seqNo:", seqNo)
-			if err := EncodeHead(buff[:headSize], &sHead); err == nil {
-				_, err = c.connReq.WriteToUDP(buff[:headSize], &hc.remote)
-			} else {
-				log.Error("EncodeHead for proccess reTrans endSession", err)
-			}
-		}
-	}
-	if c.connReq == nil {
-		return
-	}
-	lastLog := time.Now()
-	for c.Running {
-		n, remoteAddr, err := c.connReq.ReadFromUDP(c.buff)
-		if err != nil {
-			log.Error("ReadFromUDP from", remoteAddr, " ", err)
-			continue
-		}
-		c.nRecvs++
-		//c.LastRecv = time.Now().Unix()
-		if n != headSize {
-			c.nError++
-			continue
-		}
-		var head Header
-		if err := DecodeHead(c.buff[:n], &head); err != nil {
-			log.Error("DecodeHead from", remoteAddr, " ", err)
-			c.nError++
-			continue
-		}
-		if head.SeqNo >= c.seqNo {
-			log.Errorf("Invalid seq %d, server seqNo: %d", head.SeqNo, c.seqNo)
-			c.nError++
-			continue
-		}
-		if nMsg := head.MessageCnt; nMsg == 0xffff || nMsg == 0 {
-			log.Errorf("Seems msg from server MessageCnt(%d) from %s", nMsg, remoteAddr)
-			c.nError++
-			continue
-		}
-		rAddr := remoteAddr.IP.String()
-		var hc *hostControl
-		seqNext := head.SeqNo + uint64(head.MessageCnt)
-		if hh, ok := hostMap[rAddr]; ok {
-			hc = hh
-			if atomic.LoadInt32(&hc.running) == 0 {
-				hc.seqAcked = head.SeqNo
-			}
-			if time.Now().Sub(lastLog) >= time.Second {
-				log.Info(rAddr, "in process retrans for", hc.seqAcked, seqNext)
-				lastLog = time.Now()
-			}
-		} else {
-			hc = new(hostControl)
-			hc.seqAcked = head.SeqNo
-			hc.remote = *remoteAddr
-			hostMap[rAddr] = hc
-		}
-		if head.SeqNo > hc.seqAcked {
-			hc.seqAcked = head.SeqNo
-		}
-		if seqNext > hc.seqAcked+maxWindow {
-			log.Info("%d exceed maxWindow seqNo: %d", seqNext, hc.seqAcked)
-			continue
-		}
-		if atomic.LoadUint64(&hc.seqNext) < seqNext {
-			atomic.StoreUint64(&hc.seqNext, seqNext)
-		}
-
-		if atomic.LoadInt32(&hc.running) == 0 {
-			if atomic.LoadInt32(&c.nGoes) > maxGoes {
-				continue
-			}
-			nGoes := int(atomic.AddInt32(&c.nGoes, 1))
-			if nGoes > c.nMaxGoes {
-				c.nMaxGoes = nGoes
-			}
-			atomic.StoreInt32(&hc.running, 1)
-			go doReq(hc)
-		}
-	}
 }
 
 // ServerLoop	go routine multicast UDP and heartbeat
@@ -355,15 +158,4 @@ func (c *Server) ServerLoop() {
 			}
 		}
 	}
-}
-
-func (c *Server) SeqNo() int {
-	return int(c.seqNo)
-}
-
-func (c *Server) DumpStats() {
-	log.Infof("Total Sent: %d HeartBeat: %d seqNo: %d, sleep: %d\n"+
-		"Recv: %d, errors: %d, reSent: %d, maxGoes: %d",
-		c.nSent, c.nHeartBB, c.seqNo, c.nSleep, c.nRecvs, c.nError,
-		c.nResent, c.nMaxGoes)
 }
--- serverRaw.go
+++ serverRaw.go
@@ -5,42 +5,17 @@ package MoldUDP
 import (
 	"net"
 	"runtime"
-	"sync/atomic"
 	"syscall"
 	"time"
-)
-
-const (
-	maxUDPsize   = 1472
-	heartBeatInt = 2
-	maxGoes      = 512
-	maxWindow    = 120000
-	PPms         = 100 // packets per ms
 )
 
 // Server struct for MoldUDP server
 //	Running		bool
 //	Session		session for all messages
 type Server struct {
-	Session       string
-	dst           SockaddrInet4
-	fd            int
-	fdReq         int
-	PPms          int
-	Running       bool
-	endSession    bool
-	seqNo         uint64
-	endTime       int64
-	waits         int // wait for 5 seconds end of session
-	nRecvs, nSent int
-	nError        int
-	nResent       int32
-	nGoes         int32
-	nMaxGoes      int
-	nHeartBB      int
-	nSleep        int
-	msgs          []Message
-	buff          []byte
+	dst SockaddrInet4
+	fd  int
+	ServerBase
 }
 
 func (c *Server) Close() error {
@@ -49,34 +24,16 @@ func (c *Server) Close() error {
 	}
 	err := Close(c.fd)
 	c.fd = -1
+	c.ServerBase.Close()
 	return err
-}
-
-func (c *Server) EndSession(nWaits int) {
-	c.endSession = true
-	if nWaits > c.waits {
-		c.waits = nWaits
-	}
-}
-
-func (c *Server) FeedMessages(feeds []Message) {
-	c.msgs = append(c.msgs, feeds...)
 }
 
 func NewServer(udpAddr string, port int, ifName string, bLoop bool) (*Server, error) {
 	var err error
-	server := Server{seqNo: 1, waits: 5, PPms: PPms}
-	// sequence number is 1 based
-	if maddr := net.ParseIP(udpAddr); maddr != nil {
-		if maddr.IsMulticast() {
-			copy(server.dst.Addr[:], maddr.To4())
-		} else {
-			// set to 224.0.0.1
-			server.dst.Addr[0] = 224
-			server.dst.Addr[3] = 1
-		}
-	}
-	server.dst.Port = port
+	server := Server{}
+	server.ServerBase.init(udpAddr, port)
+	server.dst.Port = server.dstPort
+	copy(server.dst.Addr[:], server.dstIP.To4())
 	var ifn *net.Interface
 	if ifName != "" {
 		if ifn, err = net.InterfaceByName(ifName); err != nil {
@@ -99,22 +56,6 @@ func NewServer(udpAddr string, port int, ifName string, bLoop bool) (*Server, er
 		log.Error("syscall.Bind", err)
 		return nil, err
 	}
-	server.fdReq, err = Socket(syscall.AF_INET, syscall.SOCK_DGRAM, 0)
-	if err != nil {
-		server.fdReq = -1
-	} else {
-		fd := server.fdReq
-		SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
-		laddr.Port++
-		err = Bind(fd, &laddr)
-		if err != nil {
-			Close(server.fdReq)
-			log.Error("syscall.Bind", err)
-			server.fdReq = -1
-		} else {
-			log.Info("Request Server listen", LocalAddr(fd))
-		}
-	}
 	if err := SetMulticastInterface(fd, ifn); err != nil {
 		log.Info("set multicast interface", err)
 	}
@@ -123,154 +64,7 @@ func NewServer(udpAddr string, port int, ifName string, bLoop bool) (*Server, er
 			log.Info("set multicast loopback", err)
 		}
 	}
-	server.buff = make([]byte, 2048)
-	server.Running = true
 	return &server, nil
-}
-
-type hostControl struct {
-	remote   SockaddrInet4
-	bEnd     bool
-	seqAcked uint64
-	seqNext  uint64 // max nak sequence
-	running  int32
-}
-
-// RequestLoop		go routine process request retrans
-func (c *Server) RequestLoop() {
-	hostMap := map[string]*hostControl{}
-	var nResends int
-
-	doReq := func(hc *hostControl) {
-		//seqNo uint64, cnt uint16, remoteA net.UDPAddr
-		// only retrans one UDP packet
-		// proce reTrans
-		var buff [maxUDPsize]byte
-		seqNo := hc.seqAcked
-
-		defer atomic.AddInt32(&c.nGoes, -1)
-		nResends++
-		if (nResends & 127) == 0 {
-			log.Infof("Resend packets to %s Seq: %d -- %d", hc.remote.IP(),
-				seqNo, hc.seqNext)
-		}
-		sHead := Header{Session: c.Session, SeqNo: seqNo}
-		for seqNo < atomic.LoadUint64(&hc.seqNext) {
-			lastS := int(atomic.LoadUint64(&hc.seqNext))
-			msgCnt, bLen := Marshal(buff[headSize:], c.msgs[seqNo-1:lastS-1])
-			if msgCnt == 0 && c.endSession {
-				// end of Session
-				break
-			} else {
-				sHead.MessageCnt = uint16(msgCnt)
-			}
-			if err := EncodeHead(buff[:headSize], &sHead); err != nil {
-				log.Error("EncodeHead for proccess reTrans", err)
-				continue
-			}
-			atomic.AddInt32(&c.nResent, 1)
-			if err := Sendto(c.fdReq, buff[:headSize+bLen], 0, &hc.remote); err != nil {
-				log.Error("Res Sendto UDP", hc.remote, err)
-				break
-			}
-			seqNo += uint64(msgCnt)
-			if seqNo >= uint64(len(c.msgs)) {
-				break
-			}
-			sHead.SeqNo = seqNo
-			// system Sleep delay 50us about, so about 100us sleep
-			// or changed to Sleep 50us
-			time.Sleep(time.Microsecond * 50)
-		}
-		atomic.StoreInt32(&hc.running, 0)
-		// ony recover lost last packet and endSession
-		// using endSession Hearbeat packet to indicate endSession
-		if c.endSession && int(c.seqNo) >= len(c.msgs) && !hc.bEnd {
-			hc.bEnd = true
-			sHead.SeqNo = seqNo
-			// send endSession as well
-			sHead.MessageCnt = 0xffff
-			//log.Info("Retrans endSession, seqNo:", seqNo)
-			if err := EncodeHead(buff[:headSize], &sHead); err == nil {
-				err = Sendto(c.fdReq, buff[:headSize], 0, &hc.remote)
-			} else {
-				log.Error("EncodeHead for proccess reTrans endSession", err)
-			}
-		}
-	}
-	if c.fdReq < 0 {
-		return
-	}
-	lastLog := time.Now()
-	for c.Running {
-		n, remoteAddr, err := Recvfrom(c.fdReq, c.buff, 0)
-		if err != nil {
-			log.Error("Recvfrom", remoteAddr, " ", err)
-			continue
-		}
-		c.nRecvs++
-		//c.LastRecv = time.Now().Unix()
-		if n != headSize {
-			c.nError++
-			continue
-		}
-		var head Header
-		if err := DecodeHead(c.buff[:n], &head); err != nil {
-			log.Error("DecodeHead from", remoteAddr, " ", err)
-			c.nError++
-			continue
-		}
-		if head.SeqNo >= c.seqNo {
-			log.Errorf("Invalid seq %d, server seqNo: %d", head.SeqNo, c.seqNo)
-			c.nError++
-			continue
-		}
-		if nMsg := head.MessageCnt; nMsg == 0xffff || nMsg == 0 {
-			log.Errorf("Seems msg from server MessageCnt(%d) from %s", nMsg, remoteAddr)
-			c.nError++
-			continue
-		}
-		rAddr := remoteAddr.IP()
-		var hc *hostControl
-		seqNext := head.SeqNo + uint64(head.MessageCnt)
-		if hh, ok := hostMap[rAddr]; ok {
-			hc = hh
-			if atomic.LoadInt32(&hc.running) == 0 {
-				hc.seqAcked = head.SeqNo
-			}
-			if time.Now().Sub(lastLog) >= time.Second {
-				log.Info(rAddr, "in process retrans for", hc.seqAcked, seqNext)
-				lastLog = time.Now()
-			}
-		} else {
-			hc = new(hostControl)
-			hc.seqAcked = head.SeqNo
-			hc.remote = *remoteAddr
-			hostMap[rAddr] = hc
-		}
-		if head.SeqNo > hc.seqAcked {
-			hc.seqAcked = head.SeqNo
-		}
-		if seqNext > hc.seqAcked+maxWindow {
-			log.Info("%d exceed maxWindow seqNo: %d", seqNext, hc.seqAcked)
-			continue
-		}
-		if atomic.LoadUint64(&hc.seqNext) < seqNext {
-			atomic.StoreUint64(&hc.seqNext, seqNext)
-		}
-
-		if atomic.LoadInt32(&hc.running) == 0 {
-			if atomic.LoadInt32(&c.nGoes) > maxGoes {
-				continue
-			}
-			nGoes := int(atomic.AddInt32(&c.nGoes, 1))
-			if nGoes > c.nMaxGoes {
-				c.nMaxGoes = nGoes
-			}
-			atomic.StoreInt32(&hc.running, 1)
-			go doReq(hc)
-		}
-	}
 }
 
 // ServerLoop	go routine multicast UDP and heartbeat
@@ -356,15 +150,4 @@ func (c *Server) ServerLoop() {
 			}
 		}
 	}
-}
-
-func (c *Server) SeqNo() int {
-	return int(c.seqNo)
-}
-
-func (c *Server) DumpStats() {
-	log.Infof("Total Sent: %d HeartBeat: %d seqNo: %d, sleep: %d\n"+
-		"Recv: %d, errors: %d, reSent: %d, maxGoes: %d",
-		c.nSent, c.nHeartBB, c.seqNo, c.nSleep, c.nRecvs, c.nError,
-		c.nResent, c.nMaxGoes)
 }
